8c5695fb637988eace1bc53acf5d12ad68f3daf6
Anka <runner@sjc22-be106-33cdbebd-67c9-485c-84e0-bb26d664ef15-1E0F8D3C2221.local>
2026-03-26 14:30:11 +0000

    Update Homebrew cask to v0.2.1

--- Casks/aipaste.rb
+++ Casks/aipaste.rb
@@ -1,6 +1,6 @@
 cask "aipaste" do
-  version "0.2.0"
-  sha256 "2a18e4ad58dc3b591094f54435dec50806e7d7a2a64a0f4c995a7c0f030199ad"
+  version "0.2.1"
+  sha256 "2e09b1c80743c8292ee7021b576e8bcb76003ec09cb5f6bb0d29206f83a9914c"
 
   url "https://github.com/AiPaste/AiPaste/releases/download/v#{version}/AiPaste-#{version}-macOS.zip"
   name "AiPaste"
